7cf938844836557038fb4dcc7ccac8496d336c77
Fahad Alothman <dev.fahad.alothman@gmail.com>
2022-08-27 09:17:21 +0300

    sections 1-2

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery, useMutation } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -15,7 +17,7 @@ async function deletePost(postId) {
 
 async function updatePost(postId) {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    `https://jsonplaceholder.typicoddde.com/postId/${postId}`,
     { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
   );
   return response.json();
@@ -23,12 +25,51 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, error, isError, isLoading } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id)
+  );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation(() => updatePost(post.id));
+
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h1>L L L L L</h1>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>
+        Delete
+      </button>{" "}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error id deleting post {post.id}</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>deleting post {post.id}...</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>post {post.id} (not) deleted!</p>
+      )}
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>Error in updating post {post.id}</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>updating post {post.id}...</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>post {post.id} (not) updated!</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,21 +1,40 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(page) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
 
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Oops i did it again</h3>;
 
   return (
     <>
@@ -31,11 +50,26 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => {
+              if (prev <= 1) return prev;
+              return prev - 1;
+            });
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={!(currentPage < maxPostPage)}
+          onClick={() => {
+            setCurrentPage((prev) => {
+              if (prev < maxPostPage) return prev + 1;
+            });
+          }}
+        >
           Next page
         </button>
       </div>
